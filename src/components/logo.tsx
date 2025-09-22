dd3a13e51ef62d7ada696011e5b0bc21443c72f7
mahmoud ezz <leadbylead1@gmail.com>
2025-09-22 04:45:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 const LOGO_URL_DARK = 'https://firebasestorage.googleapis.com/v0/b/mtcmartechgooodstage-456-326b5.appspot.com/o/entrestate.com%20logo%20agaist-14.png?alt=media&token=7f6a4769-8534-4c14-b3f3-d08ca1f9aa5a';
-const LOGO_URL_LIGHT = 'https://firebasestorage.googleapis.com/v0/b/mtcmartechgooodstage-456-326b5.appspot.com/o/entrestate.com%20logo%20agaist-13.png?alt=media&token=dc10fcca-c93a-407b-aa42-2c0517d70c63';
+const LOGO_URL_LIGHT = 'https://firebasestorage.googleapis.com/v0/b/mtcmartechgooodstage-456-326b5.appspot.com/o/entrestate.com%20logo%20agaist-13-light.png?alt=media&token=5a72c1c2-3e2c-4b69-86d1-b55250468903';
 
 
 export const Logo = ({ className }: { className?: string }) => (
