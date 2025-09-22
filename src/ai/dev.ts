93f543206516d9921d705c633113e3ca679fc6b1
mahmoud ezz <leadbylead1@gmail.com>
2025-09-22 16:19:22 +0000

    ## Error Type Build Error
    ## Error Message
    Export firebase doesn't exis

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,6 @@
 
 import { genkit } from 'genkit';
-import { firebase } from '@genkit-ai/firebase';
+import firebase from '@genkit-ai/firebase';
 import { googleAI } from '@genkit-ai/googleai';
 import * as z from 'zod';
 
