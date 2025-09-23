d669c0c78f54edecbb36e73110351b3e63e00a01
mahmoud ezz <leadbylead1@gmail.com>
2025-09-23 18:53:17 +0000

    any other errors?

--- src/app/me/marketing/page.tsx
+++ src/app/me/marketing/page.tsx
@@ -1,7 +1,7 @@
 
 import { redirect } from 'next/navigation';
 
-export default function DeprecatedMarketingPage() {
+export default function MarketingPage() {
   // This page is deprecated and now permanently redirects to the new App Store.
   redirect('/apps');
 }
